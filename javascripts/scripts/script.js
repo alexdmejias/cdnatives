8bac46d4bb89410a991604aaa6b17463a3f54839
alex mejias <amejias@groupsjr.com>
2013-04-30 21:34:55 -0400

    data is pulled in from json file

--- javascripts/scripts/script.js
+++ javascripts/scripts/script.js
@@ -1,33 +1,40 @@
-jQuery(document).ready(function($) {
+$(document).ready(function(){
 	var moviePath = 'javascripts/vendor/ZeroClipboard.swf';
-
-	$('.button').on('click', function(e){
-		e.preventDefault();
-		var $this = $(this);
-		var clip = new ZeroClipboard(document.getElementById($this.attr('id')), {
-			moviePath: moviePath
-		});
-
-		clip.on( 'complete', function(client, args) {
-			alert("Copied text to clipboard: " + args.text );
-		});
+	var content = $('.content');
+	var template;
+	$.ajax({
+		url: '../../data/cdn_data.json',
+		success: function(data){
+			success(data);
+		},
+		error: function(one,two,three){
+			alert('failure loading ajax file');
+		}
 	});
 
+	function success(data) {
+		var source   = $("#entry-template").html();
+		var template = Handlebars.compile(source);
+		var temp ='';
+		for(var i = 0; i < data.length; i++) {
+			console.log(data);
+			temp += template(data[i]);
+		}
+		console.log(temp);
+		content.append(temp);
+		bind_events();
+	}
+	// will bind events to buttons, called in success()
+	function bind_events(){
+		$('.button').each(function(){
+			var $this = $(this);
+			var clip = new ZeroClipboard(document.getElementById($this.attr('id')), {
+				moviePath: moviePath
+			});
 
-	// $('.button').each(function(){
-	// 	var $this = $(this);
-
-	// 	var cdn = $this.attr('url');
-	// 	$this.attr('data-clipboard-text', cdn);
-
-	// 	var temp_clip = new ZeroClipboard(document.getElementById($this.attr('id')),
-	// 		{
-	// 			moviePath: moviePath
-	// 		}
-	// 	);
-
-	// });
-
-
-
+			clip.on( 'complete', function(client, args) {
+				alert("Copied text to clipboard: " + args.text );
+			});
+		});
+	}
 });
