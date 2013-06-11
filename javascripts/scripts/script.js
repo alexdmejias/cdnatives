a099c498367806615958ddb4f04497fa8572d8a8
alex mejias <amejias@groupsjr.com>
2013-06-11 19:48:35 -0400

    removed foundation

--- javascripts/scripts/script.js
+++ javascripts/scripts/script.js
@@ -1,26 +1,47 @@
 $(document).ready(function(){
+	var App = {
+		tags: true,
+		production: true,
+		copy: true
+	};
+	// var tags = false;
+
 	var moviePath = 'javascripts/vendor/ZeroClipboard.swf';
 	var content = $('.content');
 	var template;
 	$.ajax({
 		url: '../../data/cdn_data.json',
 		success: function(data){
-			success(data);
+			ajax_success(data);
 		},
 		error: function(one,two,three){
 			alert('failure loading ajax file');
 		}
 	});
 
-	function success(data) {
+	function toggle_button(elem) {
+		$(elem).parents('ul').children('li').children('a').removeClass('active');
+		$(elem).addClass('active');
+		console.log($(elem).parents('ul').data('option'));
+		if(App[$(elem).parents('ul').data('option')] === false) {
+			App[$(elem).parents('ul').data('option')] = true;
+		} else {
+			App[$(elem).parents('ul').data('option')] = false;
+		}
+	}
+
+	$('.option .button').on('click', function(e) {
+		e.preventDefault();
+		toggle_button(e.currentTarget);
+	});
+
+	function ajax_success(data) {
 		var source   = $("#entry-template").html();
 		var template = Handlebars.compile(source);
 		var temp ='';
 		for(var i = 0; i < data.length; i++) {
-			console.log(data);
 			temp += template(data[i]);
 		}
-		console.log(temp);
 		content.append(temp);
 		bind_events();
 	}
@@ -33,8 +54,23 @@ $(document).ready(function(){
 			});
 
 			clip.on( 'complete', function(client, args) {
-				alert("Copied text to clipboard: " + args.text );
+				show_msg(args);
 			});
 		});
+	}
+
+	function show_msg(args) {
+		var msg = args.text;
+		if(App.production === true) {
+			msg += '.min';
+		}
+		// console.log('clik');
+
+		if (App.tags === true) {
+			alert('<script src="' + msg + '.js"></script>');
+		} else {
+			alert(msg + ".js");
+		}
+
 	}
 });
