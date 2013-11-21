b2e4b25fb26acc4430523b90f2fbaad47b2e2d6e
alex mejias <alexdmejias@gmail.com>
2013-11-20 22:32:06 -0500

    added uncss and configured its task; removed less:min

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,37 +78,16 @@ module.exports = function(grunt) {
         },
         src: ['less/styles.less'],
         dest: 'css/styles.css'
-      },
-      min: {
-        options: {
-          compress: true,
-          cleancss: true,
-          report: 'min'
-        },
-        src: ['less/styles.less'],
-        dest: 'css/styles.min.css'
       }
-    },
-
-    copy: {
-      fonts: {
-        expand: true,
-        src: ["fonts/*"],
-        dest: 'dist/'
-      }
-    },
-
-    validation: {
-      options: {
-        reset: true,
-        relaxerror: [
-            "Bad value X-UA-Compatible for attribute http-equiv on element meta.",
-            "Element img is missing required attribute src."
-        ]
-      },
-      files: {
-        src: ["index.html"]
-      }
+      // min: {
+      //   options: {
+      //     compress: true,
+      //     cleancss: true,
+      //     report: 'min'
+      //   },
+      //   src: ['less/styles.less'],
+      //   dest: 'css/styles.min.css'
+      // }
     },
 
     growl: {
@@ -117,12 +96,19 @@ module.exports = function(grunt) {
       }
     },
 
-    watch: {
-      // src: {
-      //   files: '<%= jshint.src.src %>',
-      //   tasks: ['jshint:src']
-      // },
+    uncss: {
+      options: {
+        compress: true,
+        ignore: '.item'
+      },
+      dist: {
+        files: {
+          'css/styles.min.css': 'index.html'
+        }
+      }
+    },
 
+    watch: {
       less: {
         files: 'less/**/*.less',
         tasks: ['less:dev', 'growl:less']
@@ -139,21 +125,14 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-uncss');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-html-validation');
   grunt.loadNpmTasks('grunt-growl');
-
-  // Docs HTML validation task
-  grunt.registerTask('validate-html', ['validation']);
 
   // JS distribution task.
   grunt.registerTask('dist-js', ['concat', 'uglify']);
-
-  // CSS distribution task.
-  grunt.registerTask('dist-css', ['recess']);
 
 };
