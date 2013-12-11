1c408ef0b1ee6cb1319c145a5e52e102e64890f7
alex mejias <alexdmejias@gmail.com>
2013-12-10 21:19:50 -0500

    header changes; added ability to change input on cdn click

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ cdnApp.controller('cdnController', function($scope, $http) {
 	$http.get('data/cdn_data.json').success(function(data) {
 		$scope.libs = data;
 	})
+
+	$scope.toCopy = 'inital value';
+
+	$scope.sendToInput = function(w) {
+		$scope.toCopy = w;
+		console.log($scope);
+	}
 })
