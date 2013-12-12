6b258fb53772baf83b5b50eacfb66af6b75c4b34
alex mejias <alexdmejias@gmail.com>
2013-12-11 21:00:13 -0500

    text now automatically selected everytime the text changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 var cdnApp = angular.module('cdnApp', []);
 
 cdnApp.controller('cdnController', function($scope, $http) {
-	$http.get('data/cdn_data.json').success(function(data) {
-		$scope.libs = data;
-	})
+    $http.get('data/cdn_data.json').success(function(data) {
+        $scope.libs = data;
+    })
 
-	$scope.toCopy = 'inital value';
+    $scope.toCopy = 'no cdn selected';
 
-	$scope.sendToInput = function(w) {
-		$scope.toCopy = w;
-		console.log($scope);
-	}
-})
+    $scope.sendToInput = function(w) {
+        $scope.toCopy = w;
+    }
+
+});
+
+cdnApp.directive('selectOnChange', function () {
+    return function (scope, element) {
+        scope.$watch('toCopy', function(newVal, oldVal) {
+            if(oldVal === newVal){
+                return
+            }
+            element[0].select();
+        });
+    };
+});
