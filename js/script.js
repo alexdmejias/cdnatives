10113ecc1764b65e65b170225d3990b4421caaca
alex mejias <alexdmejias@gmail.com>
2014-03-04 17:25:09 -0500

    changed js and html to work with new data format

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var cdnApp = angular.module('cdnApp', []);
 cdnApp.controller('cdnController', function($scope, $http) {
     $http.get('data/cdn_data.json').success(function(data) {
         $scope.libs = data;
-    })
+    });
 
     $scope.toCopy = 'no cdn selected';
     $scope.optionTags = false;
